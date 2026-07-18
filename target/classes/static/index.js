2a530ceb382db709817dba6083c9c7243a6e6ab0
Barun <barun1234555@gmail.com>
2026-07-18 12:51:41 +0530

    random function

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,2 +1,54 @@
 // alert("hi")
 console.log("hi")
+
+data = {
+    "name":"jane",
+    "gender":"Female"
+}
+
+data.name
+
+var users = [
+    {
+        "name":"jane",
+        "gender":"Female",
+        "img":"jane.png"
+    },
+    {
+        "name":"john",
+        "gender":"Male",
+        "img":"john.png"
+    }
+]
+
+var id = 0;
+
+function toggle_user(){
+    id = (id+1)%2 ;// to switch between users
+    var user_image = document.getElementById("user_img");
+    user_image.src = users[id].img;
+    var user_gender = document.getElementById("user_gen");
+    user_gender.innerHTML = users[id].gender;
+    var user_name = document.getElementById("user_nam");
+    user_name.innerHTML = users[id].name;
+}
+
+function random_user(){
+    fetch("https://randomuser.me/api/")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        var userData = data.results[0];
+        var user_image = document.getElementById("user_img");
+        user_image.src = userData.picture.large;
+        var user_gender = document.getElementById("user_gen");
+        user_gender.innerHTML = userData.gender;
+        var user_name = document.getElementById("user_nam");
+        user_name.innerHTML = userData.name.first + " " + userData.name.last;
+    })
+    // handle error
+    .catch(function(err){
+        console.log(err);
+    })
+}   
