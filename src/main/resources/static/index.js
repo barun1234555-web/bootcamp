114a3c4cc83c2360c7f5784eb05b246c059d785f
Barun <barun1234555@gmail.com>
2026-07-18 12:50:08 +0530

    random user function

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -32,3 +32,23 @@ function toggle_user(){
     var user_name = document.getElementById("user_nam");
     user_name.innerHTML = users[id].name;
 }
+
+function random_user(){
+    fetch("https://randomuser.me/api/")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        var userData = data.results[0];
+        var user_image = document.getElementById("user_img");
+        user_image.src = userData.picture.large;
+        var user_gender = document.getElementById("user_gen");
+        user_gender.innerHTML = userData.gender;
+        var user_name = document.getElementById("user_nam");
+        user_name.innerHTML = userData.name.first + " " + userData.name.last;
+    })
+    // handle error
+    .catch(function(err){
+        console.log(err);
+    })
+}   
